4d399211c3240cc6c38a714c55c83ed3bc78ebdf
mpl <mathieu.lonjaret@gmail.com>
2016-01-21 14:49:38 +0100

    package doc: warn about deprecation
    And point to new repo

--- lock.go
+++ lock.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This package is deprecated. It has been replaced with https://go4.org/lock
 package lock
 
 import (
